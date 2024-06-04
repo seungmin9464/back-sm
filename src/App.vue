6de9c786ea9b584f770f4d570c48e88ae55013e8
BACK\82105 <https://github.com/seungmin9464/project.git>
2024-06-04 22:41:45 +0900

    reset

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <HeaderComponents />
+  <router-view />
+</template>
+
+<script>
+import HeaderComponents from "../src/components/HeaderComponents.vue";
+
+export default {
+  name: "app",
+  components: {
+    HeaderComponents,
+  },
+};
+</script>
